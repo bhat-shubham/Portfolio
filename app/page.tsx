ae724e178fde299d82b52fc3c3d8b4abb86549b8
Shubham Bhat <bhatsupshubham@gmail.com>
2025-08-12 01:46:54 +0530

    font change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 
 import Link from "next/link"
-
+import { Space_Mono } from 'next/font/google'
 export default function Home() {
   return (
 <div className="min-h-screen w-full bg-[#020617] relative">
@@ -12,37 +12,37 @@ export default function Home() {
       backgroundImage: `radial-gradient(circle 500px at 50% 200px, #3e3e3e, transparent)`,
     }}
   />
-  <div className="w-full px-110">
-     <div className="z-10 font-sans flex flex-col py-10 text-white ">
+  <div className="w-full px-105">
+     <div className="z-10 font-mono flex flex-col py-10 text-white ">
       <div>
         <h1 className="font-bold text-3xl">Shubham Bhat</h1>
       </div>
       <div className="py-5">Cybersecurity enthusiast with hands-on experience in threat detection, SOC monitoring, and bug bounties. Strong foundation in penetration testing, network forensics, and secure systems development.
-        <p className="py-4">Pune,India</p>
+        <p className="py-4 font-bold">Pune,India</p>
         <div className="flex gap-5">
           <Link href="/">
           <div className="border p-2 rounded-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-globe-icon lucide-globe"><circle cx="12" cy="12" r="10"/><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/><path d="M2 12h20"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-globe-icon lucide-globe"><circle cx="12" cy="12" r="10"/><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/><path d="M2 12h20"/></svg>
           </div>
           </Link>
           <Link href="mailto:bhatsupshubham@gmail.com">
           <div className="border p-2 rounded-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg>
           </div>
           </Link>
           <Link href="tel:+917889549228">
           <div className="border p-2 rounded-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone-icon lucide-phone"><path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-phone-icon lucide-phone"><path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384"/></svg>
           </div>
           </Link>
           <Link href="https://www.linkedin.com/in/bhatsupshubham/">
           <div className="border p-2 rounded-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
           </div>
           </Link>
           <Link href="https://github.com/bhat-shubham">
           <div className="border p-2 rounded-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github-icon lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-github-icon lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
           </div>
           </Link>
         </div>
@@ -54,21 +54,21 @@ export default function Home() {
         <h1 className="font-bold text-3xl py-2">Work Experience</h1>
         <div>
           <div className="flex align-middle gap-2 items-center">
-          <h1 className="text-2xl font-bold">Presidet@CodeX Club</h1>
+          <h1 className="text-xl font-bold">Presidet@CodeX Club</h1>
           <p className="bg-white/10 p-1 rounded-md">Pune,India</p>
           </div>
         <p>Cybersecurity enthusiast with hands-on experience in threat detection, SOC monitoring, and bug bounties for threat identification. Strong foundation in penetration testing, network forensics, and secure systems development. Passionate about solving real-world security challenges through code and automation.</p>
         </div>
         <div>
           <div className="flex align-middle gap-2 items-center">
-          <h1 className="text-2xl font-bold">Contributor@Girlscript summer of code</h1>
+          <h1 className="text-xl font-bold">Contributor@Girlscript summer of code</h1>
           <p className="bg-white/10 p-1 rounded-md">Remote</p>
           </div>
         <p>Contributed to various open-source projects, demonstrating technical skills. Ranked #841 among 40,000+ Global Coders.</p>
         </div>
         <div>
           <div className="flex align-middle gap-2 items-center">
-          <h1 className="text-2xl font-bold">IBM SkillsBuild AI/ML Intern@EduNet Foundation</h1>
+          <h1 className="text-xl font-bold">IBM SkillsBuild AI/ML Intern@EduNet Foundation</h1>
           <p className="bg-white/10 p-1 rounded-md">Pune,India</p>
           </div>
         <p>Identified potential security threats and automated the identification of suspicious activities. Engineered and deployed machine learning models that increased predictive accuracy by 30%, utilizing Python libraries such as TensorFlow and scikit-learn.</p>
@@ -78,14 +78,14 @@ export default function Home() {
         <h1 className="font-bold text-3xl py-2">Education</h1>
         <div>
           <div className="flex align-middle gap-2 items-center">
-          <h1 className="text-2xl font-bold">PDEA&apos;s College Of Engineering, Manjari</h1>
+          <h1 className="text-xl font-bold">PDEA&apos;s College Of Engineering, Manjari</h1>
           <p className="bg-white/10 p-1 rounded-md">2022-2026</p>
           </div>
         <p>Bachelor&apos;s Of Engineering In Information Technology</p>
         </div>
         <div>
           <div className="flex align-middle gap-2 items-center">
-          <h1 className="text-2xl font-bold">Kendriya Vidyalaya No.2</h1>
+          <h1 className="text-xl font-bold">Kendriya Vidyalaya No.2</h1>
           <p className="bg-white/10 p-1 rounded-md">2018-2024</p>
           </div>
         <p>High School, Minor in Computer Science</p>
@@ -94,7 +94,7 @@ export default function Home() {
         <div className="flex flex-col gap-5">
         <h1 className="font-bold text-3xl py-2">Skills</h1>
         <div>
-          <div className="flex gap-2 flex-wrap">
+          <div className="flex gap-3 flex-wrap">
           <p className="bg-white/10 p-1 rounded-md">Vulnerability Assessment</p>
           <p className="bg-white/10 p-1 rounded-md">Incident Response</p>
           <p className="bg-white/10 p-1 rounded-md">Threat Intelligence</p>
